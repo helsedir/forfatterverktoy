13ad132ed73e9a07d3c6a75a857176798ec9285d
Goran Karabeg <goran.karabeg@gmail.com>
2014-09-19 08:57:54 +0200

    Added text-angular

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -23,5 +23,5 @@ angular.module('webUiApp')
 		  $location.path('/guideline/0');
   	}; 
 
-    
+    $scope.textAngularOpts = {};
   }]);
--- app/scripts/textangular/textAngularSetup.js
+++ app/scripts/textangular/textAngularSetup.js
@@ -14,7 +14,7 @@ angular.module('textAngularSetup', [])
 		['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre', 'quote'],
 		['bold', 'italics', 'underline', 'strikeThrough', 'ul', 'ol', 'redo', 'undo', 'clear'],
 		['justifyLeft','justifyCenter','justifyRight','indent','outdent'],
-		['html', 'insertImage', 'insertLink', 'insertVideo']
+		['html', 'insertLink']
 	],
 	classes: {
 		focussed: "focussed",
