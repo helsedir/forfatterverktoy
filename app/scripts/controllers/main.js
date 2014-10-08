c49a23d2b22eb823bdc4a5534363bae00b0586a8
Sjur Seibt <sjurlur@gmail.com>
2014-10-08 13:51:16 +0200

    remove guideline from index

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -23,6 +23,44 @@ angular.module('webUiApp')
 		  $location.path('/guideline/0');
   	}; 
 
+    $scope.editGuidelineBtnClick = function(index){
+      $location.path('/guideline/' + index);
+    };
+
+    $scope.deleteGuidelineBtnClick = function(index){
+      var guidelineToDelete = $scope.guidelines[index];
+      Guideline.delete({ _id: guidelineToDelete.guidelineId })
+      .$promise.then(function(){
+        toastr.success(guidelineToDelete.title, 'Slettet');
+        $scope.guidelines.splice(index, 1);
+      }, function(error){
+        if(error.status == 401)
+        {
+          toastr.warning('Logg inn for å slette');
+        }
+        else
+        {
+          toastr.error('Status code: ' + error.status +' '+ error.statusText + ' Error data: ' + error.data.message, 'Error!');
+        }
+      });
+    };
+
     $scope.textAngularOpts = {};
 
-  }]);
+  }]).directive('ngConfirmClick', [
+  function(){
+    return {
+      priority: -1,
+      restrict: 'A',
+      link: function(scope, element, attrs){
+        element.bind('click', function(e){
+          var message = attrs.ngConfirmClick;
+          if(message && !confirm(message)){
+            e.stopImmediatePropagation();
+            e.preventDefault();
+          }
+        });
+      }
+    };
+  }
+]);
