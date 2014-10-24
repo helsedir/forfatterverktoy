f754f91cd777a152db2e9143f4a4f471e8930e05
Sjur Seibt <sjurlur@gmail.com>
2014-10-24 12:24:18 +0200

    move searchbox

---
+++ app/scripts/directives/sortorder.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name webUiApp.directive:sortOrder
+ * @description
+ * # sortOrder
+ */
+angular.module('webUiApp')
+  .directive('sortOrder', function () {
+    return {
+      template: '<button type="button" class="btn btn-default" ng-click="editOrderBtnClick()">Rediger rekkefølgen</button>',
+      restrict: 'A',
+      replace: true,
+      link: function postLink(scope, element, attrs) {
+        
+      }
+    };
+  });
