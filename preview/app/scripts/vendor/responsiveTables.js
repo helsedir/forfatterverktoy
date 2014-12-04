6aa8d46b4647fad1810617c8ae20d862d140273d
Sjur Seibt <sjurlur@gmail.com>
2014-12-04 11:56:22 +0100

    responsive tables

--- preview/app/scripts/vendor/responsiveTables.js
+++ preview/app/scripts/vendor/responsiveTables.js
@@ -1,19 +1,41 @@
 function responsiveTables() {
 
-  loopTables();
+  loopTables(document);
   //When accordions are opened
-  $('.recommendationheader, .aboutguidelineicon, .rationallink, .practicaladvicelink').click(function () {
+  $('.recommendationheader, .aboutguidelineicon, .rationallink, .practicaladvicelink, .picosummarylink, .resp-accordion').click(function () {
+
+    var context;
+
+    if ($(this).hasClass('recommendationheader')) {
+      context = $(this).closest('.Recommendation').find('.textContainer');
+    }
+
+    else if ($(this).hasClass('aboutguidelineicon')) {
+      context = $(this).closest('.pageheader').find('.aboutexpand');
+    }
+
+    else if ($(this).hasClass('picosummarylink')) {
+      context = $(document).find('.picosummary');
+    }
+
+    /*else if ($(this).find('.rationallink') || $(this).find('.practicaladvicelink')) {
+      context = $(this).closest('.tabs').find('.resp-tabs-container');
+
+    }*/
+
     window.setTimeout(function(){
-      loopTables();
-    }, 10);
+      loopTables(context);
+    }, 100); //Timeout function can be removed if not using angular
   })
 }
 
-function loopTables() {
-  $('table').each(function (index) { //find all tables
+function loopTables(context) {
+  $(context).find('table').each(function (index) { //find all tables
     var width = $(this).width();
     var containerWidth = $(this).parent().width();
-    if (width > containerWidth) {
+    console.log('width: '+width+' container: '+containerWidth);
+    console.log(this);
+    if (containerWidth > 0 && width > containerWidth) {
       addDataHeaders(this); //prepare table for breaking
       $(this).addClass('breakTable');
     }
@@ -24,6 +46,9 @@ function addDataHeaders(table) {
   var headers = [];
 
   $(table).find('tr').each(function (yindex) {
+    if (yindex == 0) {
+      $(this).addClass('hidden'); //always hide first row when breaking
+    }
     $(this).find('td').each(function (xindex) {
       if (yindex == 0) {
         headers.push(this.innerText);
